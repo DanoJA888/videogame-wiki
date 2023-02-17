81bd7676785a8405c4098394f849cd4c3d6c9549
Sebastian <sebastian_balderrama@techexchange.in>
2023-02-17 04:26:29 +0000

    implemented the get_wiki_page() and get_all_wiki_names() methods in Backend

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,14 +1,31 @@
 # TODO(Project 1): Implement Backend according to the requirements.
+
+from google.cloud import storage
+
 class Backend:
 
     def __init__(self):
         pass
         
+    # Returns the requested page
     def get_wiki_page(self, name):
-        pass
+        storage_client = storage.Client()
+        try:
+            bucket = storage_client.bucket('wikicontent')
+        except google.cloud.exceptions.NotFound:
+            return 'bucket not found'
+        blob = bucket.blob(name)
+        return blob if blob else 'page not found'
 
+    # Returns a list of all the page names
     def get_all_page_names(self):
-        pass
+        storage_client = storage.Client()
+        try:
+            bucket = storage_client.bucket('wikicontent')
+        except google.cloud.exceptions.NotFound:
+            return 'bucket not found'
+        blobs = bucket.list_blobs()
+        return [blob.name for blob in blobs]
 
     def upload(self):
         pass
@@ -21,4 +38,3 @@ class Backend:
 
     def get_image(self):
         pass
-
