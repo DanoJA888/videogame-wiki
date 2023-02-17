a6b9c330a9570b0d5d3889f8cb9f70cb2ab87d8b
Sebastian <sebastian_balderrama@techexchange.in>
2023-02-17 05:09:39 +0000

    added routes to pages.py that show output of get_wiki_page() and get_all_page_names() in Backend

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -1,5 +1,5 @@
-from flask import render_template
-
+from flask import render_template, redirect
+from flaskr.backend import Backend
 
 def make_endpoints(app):
 
@@ -12,3 +12,16 @@ def make_endpoints(app):
         return render_template('main.html')
 
     # TODO(Project 1): Implement additional routes according to the project requirements.
+
+    @app.route("/get-page/<page_name>")
+    def get_wiki_page(page_name):
+        b = Backend()
+        page = b.get_wiki_page(page_name)
+        with page.open('r') as f:
+            return f.read()
+
+    @app.route("/get-pages")
+    def get_all_page_names():
+        b = Backend()
+        all_page_names = b.get_all_page_names()
+        return ', '.join(all_page_names)
