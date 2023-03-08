3e0046994d3fb1a1db199d76d0c788ddca077911
Sebastian Balderrama <sebastian_balderrama@techexchange.in>
2023-03-08 01:31:59 -0700

    uncommented code

--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -19,31 +19,31 @@ def client(app):
 
 # TODO(Checkpoint (groups of 4 only) Requirement 4): Change test to
 # match the changes made in the other Checkpoint Requirements.
-# '''
-# def test_home_page(client):
-#     resp = client.get("/")
-#     assert resp.status_code == 200
-#     assert b"Hello, World!\n" in resp.data
-# '''
+'''
+def test_home_page(client):
+    resp = client.get("/")
+    assert resp.status_code == 200
+    assert b"Hello, World!\n" in resp.data
+'''
 
-# # TODO(Project 1): Write tests for other routes.
-# def test_pages_route(client):
-#     resp = client.get("/pages/")
-#     html = resp.data.decode()
-#     assert resp.status_code == 200
+# TODO(Project 1): Write tests for other routes.
+def test_pages_route(client):
+    resp = client.get("/pages/")
+    html = resp.data.decode()
+    assert resp.status_code == 200
 
-# def test_signup_route(client):
-#     resp = client.get("/signup")
-#     assert resp.status_code == 200
+def test_signup_route(client):
+    resp = client.get("/signup")
+    assert resp.status_code == 200
 
-# @mock.patch('flask_login.utils._get_user')
-# def test_login_required(self, client):
-#     user = mock.MagicMock()
-#     resp_upload = client.get("/upload")
-#     resp_logout = client.get("/logout")
-#     if not user.is_authenticated():
-#         assert resp_upload.status_code == 401
-#         assert resp_logout.status_code == 401
+@mock.patch('flask_login.utils._get_user')
+def test_login_required(self, client):
+    user = mock.MagicMock()
+    resp_upload = client.get("/upload")
+    resp_logout = client.get("/logout")
+    if not user.is_authenticated():
+        assert resp_upload.status_code == 401
+        assert resp_logout.status_code == 401
 
 def test_get_user_page_route(client):
     page = 'sports_games.html'
