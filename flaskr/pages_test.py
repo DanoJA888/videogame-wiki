dede92401e2fb4b5b3591a32ba3e1d9329809bf7
Sebastian Balderrama <sebastian_balderrama@techexchange.in>
2023-03-08 01:25:37 -0700

    added tests for get_user_page route and upload route

--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -19,31 +19,57 @@ def client(app):
 
 # TODO(Checkpoint (groups of 4 only) Requirement 4): Change test to
 # match the changes made in the other Checkpoint Requirements.
-'''
-def test_home_page(client):
-    resp = client.get("/")
-    assert resp.status_code == 200
-    assert b"Hello, World!\n" in resp.data
-'''
+# '''
+# def test_home_page(client):
+#     resp = client.get("/")
+#     assert resp.status_code == 200
+#     assert b"Hello, World!\n" in resp.data
+# '''
 
-# TODO(Project 1): Write tests for other routes.
-def test_pages_route(client):
-    resp = client.get("/pages/")
-    html = resp.data.decode()
-    assert resp.status_code == 200
+# # TODO(Project 1): Write tests for other routes.
+# def test_pages_route(client):
+#     resp = client.get("/pages/")
+#     html = resp.data.decode()
+#     assert resp.status_code == 200
 
-def test_signup_route(client):
-    resp = client.get("/signup")
-    assert resp.status_code == 200
+# def test_signup_route(client):
+#     resp = client.get("/signup")
+#     assert resp.status_code == 200
 
-@mock.patch('flask_login.utils._get_user')
-def test_login_required(self, client):
-    user = mock.MagicMock()
-    resp_upload = client.get("/upload")
-    resp_logout = client.get("/logout")
-    if not user.is_authenticated():
-        assert resp_upload.status_code == 401
-        assert resp_logout.status_code == 401
+# @mock.patch('flask_login.utils._get_user')
+# def test_login_required(self, client):
+#     user = mock.MagicMock()
+#     resp_upload = client.get("/upload")
+#     resp_logout = client.get("/logout")
+#     if not user.is_authenticated():
+#         assert resp_upload.status_code == 401
+#         assert resp_logout.status_code == 401
+
+def test_get_user_page_route(client):
+    page = 'sports_games.html'
+    resp = client.get(f'/pages/{page}')
+    assert resp.status_code == 200
+    assert b"FIFA" in resp.data
+
+def test_upload_route_GET_logged_out(client):
+    resp = client.get('/upload')
+    assert resp.status_code == 401
+
+def test_upload_route_GET_logged_in(client):
+    client.post('/login', data={ 'username' : 'sebastian', 'password' : 'password' })
+    resp = client.get('/upload')
+    assert resp.status_code == 200
+    assert b"value=Upload" in resp.data
+
+def test_upload_route_POST_logged_out(client):
+    resp = client.post('/upload', data={ 'file' : 'test'})
+    assert resp.status_code == 401
+
+def test_upload_route_POST_logged_in(client):
+    client.post('/login', data={ 'username' : 'sebastian', 'password' : 'password' })
+    resp = client.post('/upload', data={ 'file' : ''})
+    assert resp.status_code == 302
+
 
 
 
